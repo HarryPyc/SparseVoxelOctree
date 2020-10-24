10e0e96db5e9ba8e6bb1bae664eaa7df9007ae7c
HarryPyc <harrypan311@gmail.com>
2020-10-24 16:21:51 -0400

    Efficient Raycasting

--- SparseVoxelOctree/SparseVoxelOctree/Octree.cu
+++ SparseVoxelOctree/SparseVoxelOctree/Octree.cu
@@ -31,7 +31,7 @@ __global__ void MarkKernel(Node* d_node, uint* d_idx) {
 	glm::uvec3 idx = ConvUintToUvec3(d_idx[x]), _idx = glm::uvec3(0);
 
 	for (uint i = 0; i <= curLevel; i++) {
-		_idx = idx % glm::uvec3(1 << (maxLevel - i + 1)) / glm::uvec3(1 << (maxLevel - i));
+		_idx = idx % glm::uvec3(1 << (maxLevel - i)) / glm::uvec3(1 << (maxLevel - i - 1));
 		nodeIdx = d_node[nodeIdx].ptr + _idx.x + _idx.y * 2 + _idx.z * 4;
 		if (d_node[nodeIdx].ptr == NULLPTR || d_node[nodeIdx].ptr == MARKED) {
 			d_node[nodeIdx].ptr = MARKED;
@@ -58,7 +58,7 @@ __global__ void MemcpyVoxelToLeafNodeKernel(Node* d_node, Voxel* voxelSrc, uint*
 	glm::uvec3 idx = ConvUintToUvec3(d_idx[x]), _idx = glm::uvec3(0);
 
 	for (uint i = 0; i < maxLevel; i++) {
-		_idx = idx % glm::uvec3(1 << (maxLevel - i + 1)) / glm::uvec3(1 << (maxLevel - i));
+		_idx = idx % glm::uvec3(1 << (maxLevel - i)) / glm::uvec3(1 << (maxLevel - i - 1));
 		//printf("[%i,%i,%i]\n", _idx.x, _idx.y, _idx.z);
 		nodeIdx = d_node[nodeIdx].ptr + _idx.x + _idx.y * 2 + _idx.z * 4;
 	}
@@ -178,22 +178,28 @@ struct Ray {
 
 		float tmin = glm::max(-999.f, glm::max(tsmaller[0], glm::max(tsmaller[1], tsmaller[2])));
 		float tmax = glm::min(999.f, glm::min(tbigger[0], glm::min(tbigger[1], tbigger[2])));
-		t = tmin;
+		t = (tmin + tmax) / 2.f;
 		return (tmin < tmax);
 	}
 };
-
+struct HitInfo {
+	glm::uvec3 idx;
+	float t;
+	__device__ HitInfo() {};
+	__device__ HitInfo(glm::uvec3 _idx, float _t) : idx(_idx), t(_t) {};
+};
 __device__ Voxel OctreeTraverse(Node* d_node, Node root, Ray ray, glm::vec3 minAABB, uint currentLevel, uint targetLevel, float& t) {
 	if (root.ptr == NULLPTR)
 		return root.voxel;
 	if (currentLevel == targetLevel)
 		return root.voxel;
-	if (root.ptr == MARKED)
-		printf("sb\n");
+
 	currentLevel++;
+	HitInfo hits[8];
+	int counter = 0;
 	float temp = 999.f;
 	Voxel res;
-	const float delta = d_Info.delta / float((1 << (currentLevel - 1)));
+	const float delta = d_Info.delta / float((1 << (currentLevel)));
 	for (int i = 0; i < 2; i++)
 		for (int j = 0; j < 2; j++)
 			for (int k = 0; k < 2; k++) {
@@ -201,18 +207,28 @@ __device__ Voxel OctreeTraverse(Node* d_node, Node root, Ray ray, glm::vec3 minA
 				glm::vec3 _minAABB = minAABB + glm::vec3(idx) * delta;
 				float _t;
 				if (ray.RayAABBIntersection(_minAABB, _minAABB + delta, _t)) {
-					if (_t > temp) continue;
-					Node _root = d_node[root.ptr + idx.x + idx.y * 2 + idx.z * 4];
-					//printf("curLevel: %i, root.ptr: %u\n", currentLevel, root.ptr);
-					Voxel voxel = OctreeTraverse(d_node,  _root, ray, _minAABB, currentLevel, targetLevel, t);
-					if (!voxel.empty() && _t < temp) {
-						temp = _t;
-						res = voxel;
-						if(currentLevel == targetLevel)
-							t = _t;
-					}
+					HitInfo hit(idx, _t);
+					hits[counter++] = hit;
 				}
 			}
+	for(int i = 0; i < counter - 1; i++)
+		for (int j = 0; j < counter - i - 1; j++) {
+			if (hits[j].t > hits[j + 1].t) {
+				HitInfo temp = hits[j];
+				hits[j] = hits[j + 1];
+				hits[j + 1] = temp;
+			}
+		}
+	for (int i = 0; i < counter; i++) {
+		Node _root = d_node[root.ptr + hits[i].idx.x + hits[i].idx.y * 2 + hits[i].idx.z * 4];
+		glm::vec3 _minAABB = minAABB + glm::vec3(hits[i].idx) * delta;
+		Voxel voxel = OctreeTraverse(d_node, _root, ray, _minAABB, currentLevel, targetLevel, t);
+		if (!voxel.empty()) {
+			if (currentLevel == targetLevel)
+				t = hits[i].t;
+			return voxel;
+		}
+	}
 	return res;
 }
 __global__ void RayCastKernel(uint* d_pbo, Node* d_node, const uint w, const uint h) {
